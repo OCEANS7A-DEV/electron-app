e0e5d6160c043e4b45de28ef4aa7dcdc2da8928f
OCEANS7A-DEV <oceans7a@gmail.com>
2025-05-23 12:04:31 +0900

    t

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,17 +4,32 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      outDir: 'out/main',
+      rollupOptions: {
+        input: resolve(__dirname, 'src/main/index.ts'),
+      }
+    }
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      outDir: 'out/preload',
+      rollupOptions: {
+        input: resolve(__dirname, 'src/preload/index.ts'), // 必要に応じて修正
+      }
+    }
   },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
       }
     },
-    plugins: [vue()]
+    plugins: [vue()],
+    build: {
+      outDir: 'out/renderer'
+    }
   }
 })
